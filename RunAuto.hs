ec1627cd8ba21b25776b1a9cf448d7d2a9964946
Jan Kopański <j.kopanski@students.mimuw.edu.pl>
2017-03-19 10:50:49 +0100

    read without error

--- RunAuto.hs
+++ RunAuto.hs
@@ -1,2 +1,25 @@
-import Auto
-import Text.Read
+import Auto ()
+import System.IO
+import System.Environment
+-- import Text.Read
+import Numeric.Natural
+
+main :: IO ()
+main = do
+  [filePath] <- getArgs
+  handle <- openFile filePath ReadMode
+  contents <- hGetContents handle
+  let x = parseInput contents
+  print x
+  hClose handle
+
+
+parseInput :: String -> (([Natural], [Natural], [Natural], [(Natural, Char, [Natural])]), String)
+parseInput input = ((st', is', ia', tl), w)
+  where
+    st:is:ia:xs = filter (not . null) $ lines input
+    st' = read st :: [Natural]
+    is' = read is :: [Natural]
+    ia' = read ia :: [Natural]
+    w = last xs
+    tl = map (\l -> read l :: (Natural, Char, [Natural])) (init xs)
